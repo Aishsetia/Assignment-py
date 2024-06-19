6766cd061d58558d90d09985472466d52b7d812a
Aishsetia <setiaaishwarya@gmail.com>
2024-06-19 10:48:51 -0700

    changes

--- q24.py
+++ q24.py
@@ -5,13 +5,14 @@ num1=int(input("enter a number"))
 num2=int(input("enter a number"))
 operator=input("enter operand")
 
-if +==operator:
-   output=num1+num2
-'''elif operand==-:
-     output=num1-num2
-elif operand==*:
+if operator=='+':
+    output=num1+num2
+elif operator=='-':
+    output=num1-num2
+elif operator=='*':
      output=num1*num2
 else:
-    output=num1/num2'''
+    output=num1/num2
+print(output)
      
 print(output)
